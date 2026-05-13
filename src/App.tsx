a128da5a22843230d9c0c8d4a05dbe6d3562f401
fridayvibebanger-debug <fridayvibebanger@gmail.com>
2026-05-13 09:48:42 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ import ScreenshotPolicy from './pages/policies/ScreenshotPolicy';
 import CommunityRules from './pages/policies/CommunityRules';
 import RecruiterPortalPage from './pages/RecruiterPortalPage';
 import TestAI from './pages/TestAI';
-import LiveNotificationListener from '@/components/notifications/LiveNotificationListener';
+import LiveNotificationListener from '@/components/LiveNotificationListener';
 import AiUtilsTest from './pages/AiUtilsTest';
 import CareerAIPage from './pages/CareerAIPage';
 import NotificationsPage from './pages/NotificationsPage';
