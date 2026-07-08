1b389ad542fa88d620f06ff54ff5aa1b3a9dd184
fridayvibebanger-debug <fridayvibebanger@gmail.com>
2026-07-08 16:26:37 +0200

    Create SubscriptionModal.tsx

---
+++ src/components/SubscriptionModal.tsx
@@ -0,0 +1,13 @@
+export default function SubscriptionModal({
+  currentTier,
+  onClose,
+}: {
+  currentTier: string;
+  onClose: () => void;
+}) {
+  return (
+    <div>
+      {/* Subscription UI */}
+    </div>
+  );
+}
